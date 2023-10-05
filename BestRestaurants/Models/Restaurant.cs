c1f262e72b9b8941cf1747ca84464e4eecf2f5f2
WillJolley <yeswilljolley@gmail.com>
2023-10-04 21:08:59 -0700

    review display stuff

--- BestRestaurants/Models/Restaurant.cs
+++ BestRestaurants/Models/Restaurant.cs
@@ -7,7 +7,6 @@ namespace BestRestaurants.Models
   {
     public int RestaurantId { get; set; } 
     public string Name { get; set; }
-    public string Description { get; set; }
     public int CuisineId { get; set; }
     public Cuisine Cuisine { get; set; }
     public List<Review> Reviews { get; set; }
